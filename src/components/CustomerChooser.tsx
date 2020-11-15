851a9afde88cc55f9b229ffeb878e1642ffe10ed
Max Carroll <max.carroll@barbal.co>
2020-11-15 21:39:42 +0000

    implemented picker for CustomerChooser

--- src/components/CustomerChooser.tsx
+++ src/components/CustomerChooser.tsx
@@ -1,23 +1,34 @@
-import { TextField } from '@material-ui/core'
-import * as React from 'react'
-import { ChangeEvent, Dispatch, SetStateAction } from 'react'
-import { ProductDto } from '../models/ProductDto'
-import { CustomerDto } from '../models/CustomerDto'
+import { TextField } from "@material-ui/core";
+import * as React from "react";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
+import { ProductDto } from "../models/ProductDto";
+import { CustomerDto } from "../models/CustomerDto";
+import Autocomplete from "@material-ui/lab/Autocomplete/Autocomplete";
 
 export interface CustomerChooserProps {
-    customers: Array<CustomerDto>
-    selected?: CustomerDto
-    setSelected: Dispatch<SetStateAction<CustomerDto| undefined>> 
+  customers: Array<CustomerDto>;
+  selected?: CustomerDto;
+  setSelected: Dispatch<SetStateAction<CustomerDto | null>>;
 }
 
-export const CustomerChooser = ({customers, selected, setSelected} : CustomerChooserProps)=> {
-
-    
-
-
-    return (
+export const CustomerChooser = ({
+  customers,
+  selected,
+  setSelected,
+}: CustomerChooserProps) => {
+  return (
     <>
-        {customers.map(c=> <h2>{c.name}</h2>)}
-    </> 
-    )
-}
+      <Autocomplete
+        id="combo-box-demo"
+        options={customers}
+        getOptionLabel={(option) => option.name}
+        style={{ width: 300 }}
+        value={selected}
+        renderInput={(params) => (
+          <TextField {...params} label="Combo box" variant="outlined" />
+        )}
+        onChange={(e, v) => setSelected(v)}
+      />
+    </>
+  );
+};
